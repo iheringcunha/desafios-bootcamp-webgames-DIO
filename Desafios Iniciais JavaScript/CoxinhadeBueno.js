9925a785e18fb8a63cf7b5ac5f2d95745daf3b78
iheringcunha <iheringcunha91@gmail.com>
2022-04-30 15:02:55 -0300

    Resolução dos desafios iniciais JavaScript

---
+++ Desafios Iniciais JavaScript/CoxinhadeBueno.js
@@ -0,0 +1,8 @@
+//TODO: Complete o código com uma solução possível
+let line = gets().split(" ");
+let H = parseInt(line[0]);
+let P = parseInt(line[1]);
+
+let media = (H/P).toFixed(2);
+
+console.log(media);
